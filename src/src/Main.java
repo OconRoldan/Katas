d1c4ccace9f06bd547b8e3e122f3823caabac80d
OconRoldan <oconroldan@hotmail.com>
2023-01-13 13:47:23 +0100

    PruebaFinal

--- src/src/Main.java
+++ src/src/Main.java
@@ -7,13 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		/**
-		 * hola
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * adios
+		 * Mis Katas
 		 * 
 		 */
 		String s = "holaqef";
